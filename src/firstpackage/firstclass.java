aa20f0ea216a603469c8938c59204bf604b74c7e
Diana Leca <dianaleca@Dianas-MacBook-Air.local>
2022-02-20 14:17:58 +0200

    first learning java script

---
+++ src/firstpackage/firstclass.java
@@ -0,0 +1,13 @@
+package firstpackage;
+
+public class firstclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   System.out.println("This is a test"); 
+		   System.out.println("Learnining Java");
+		 
+
+	}
+
+}
